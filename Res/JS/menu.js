9ffe050260216d34bde13f2b9584aeb4860e0625
brenkr <robin.renker@iet-gibb.ch>
2016-11-06 21:11:47 +0100

    Menu map update

--- Res/JS/menu.js
+++ Res/JS/menu.js
@@ -1,17 +1,25 @@
 //########################################################################
 //Menu Var
-menu_side_width_def = 50;
-menu_side_width_intent = 100;
+
 //########################################################################
-$('#menu_side').hover(function () {
-    $('#menu_side').animate({width: menu_side_width_intent},{queue:false,duration:100});
-});
-$('#menu_side').mouseleave(function () {
-    $('#menu_side').animate({width: menu_side_width_def},{queue:false,duration:100});
-});
 $(window).resize(function () {
-    $('#menu_side').height($(window).height()-$('#menu_top').height());
-    $('#menu_side').css({top:$('#menu_top').height()});
+
+});
+$('#menu_create_map').mousedown(function () {
+    try{
+        var x = $('#menu_create_map_x').val();
+        var y = $('#menu_create_map_y').val();
+        if($.isNumeric(x) && $.isNumeric(y)){
+            x = parseInt(x);
+            y = parseInt(y);
+            map = map_create_random(y, x);
+            map_display(map);
+            map_control_resize(0);
+        }
+    }
+    catch (error){
+
+    }
 });
 //########################################################################
 function menu_side_hide() {
